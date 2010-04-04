3c0ee048ecc70d432a15b839d21125413998927f
hairmare <hairmare@punk.valinor>
2010-04-04 15:22:41 +0200

    version with working ticker, runs on dev.
    this version adds ajax loading of ticker data
    all thats missing now is a scroller for to
    long data. i also added basic history support.
    it doesnt survive reloads, but it basically is
    there. facebook support is now mature enough
    to request publishing rights, next ill start
    testing updating statuses.

--- htdocs/rabe.js
+++ htdocs/rabe.js
@@ -1,12 +1,26 @@
 var SONGTICKERLI_FB = function() {
 };
 SONGTICKERLI_FB.init = function() {
-	FB.init('0b69cbfe795f13e8f415fee381e2d695', 'xd_receiver.html');
+	FB.init('0b69cbfe795f13e8f415fee381e2d695', 'fb_xd_receiver.html');
 	FB.ensureInit(function() {
 		SONGTICKERLI_FB.initialized = true;
-		SONGTICKERLI_FB.register();
+		if (typeof SONGTICKERLI_FB.registered != 'undefined' || SONGTICKERLI_FB.registered != true) {
+			SONGTICKERLI_FB.register();
+		}
 	});
 }
+SONGTICKERLI_FB.jStoreReady = function() {
+	fb_perms = jQuery.jStore.get('facebook_perms');
+	fb_url = jQuery.jStore.get('facebook_url');
+	if (fb_perms != '') {
+		$('#songtickerli p.facebook-btn').slideUp();
+		$('#songtickerli a.facebook-profile-btn').hide();
+		SONGTICKERLI_FB.registered = true;
+		SONGTICKERLI_FB.init();
+	} else {
+		$('#songtickerli a.facebook-profile-btn').show();
+	}
+};
 SONGTICKERLI_FB.post = function(track) {
 	FB.Connect.streamPublish(track.artist + ' ' + track.title);
 };
@@ -24,7 +38,10 @@ SONGTICKERLI_FB.register_callback_cancel = function() {
 SONGTICKERLI_FB.register_callback_perms = function(perms) {
 	if (perms) {
 		jQuery.jStore.set('facebook_perms', perms);
+		jQuery.jStore.set('facebook_username', FB.Connect.get_loggedInUser());
+		jQuery.jStore.set('facebook_url', 'http://www.facebook.com/'+FB.Connect.get_loggedInUser());
 		$('.facebook-btn').hide();
+		SONGTICKERLI_FB.registered = true;
 	}
 };
 
@@ -33,41 +50,141 @@ var SONGTICKERLI_TWITTER = function() {
 SONGTICKERLI_TWITTER.post = function(track) {
 };
 
+/**
+ * basic container function for songtickerli
+ *
+ * no private vars whatsoever, since i dont plan
+ * on supporting multiple instances of this anyway.
+ */
 var SONGTICKERLI = function() {
 	var storageReady = false;
-	var showLoveDialog = true;
-	var observers = [];
-
 };
+SONGTICKERLI.showLoveDialog = true;
+SONGTICKERLI.observers = [];
 SONGTICKERLI.artist = 'songticker.li';
 SONGTICKERLI.title = 'inializing';
+SONGTICKERLI.messate = null;
+SONGTICKERLI.starttime = '';
+SONGTICKERLI.delay = 0;
+/**
+ * an array for defining some basic setting fields to load
+ */
 SONGTICKERLI.configDataFields = [
 	{name: 'showLoveDialog', field: SONGTICKERLI.showLoveDialog},
 	{name: 'username', field: '#songtickerli input[name=username]'}
 ];
+/**
+ * register an observer 
+ */
 SONGTICKERLI.register_observer = function(observer) {
-	$(SONGTICKERLI.observers).append(observer);
+	SONGTICKERLI.observers[SONGTICKERLI.observers++] = observer;
 };
+/**
+ * call observers that support the love method
+ */
+SONGTICKERLI.observer_love = function(track) {
+	for (var i = 0; i < SONGTICKERLI.observers.length(); i++) {
+		if (typeof SONGTICKERLI.observers[i].love == 'function') {
+			SONGTICKERLI.observers[i].love(track);
+		}
+	}
+};
+/**
+ * call observers that support the jStoreReady method for loading data
+ */
+SONGTICKERLI.observer_jStoreReady = function() {
+	for (var i = 0; i < SONGTICKERLI.observers.length(); i++) {
+		if (typeof SONGTICKERLI.observers[i].jStoreReady == 'function') {
+			SONGTICKERLI.observers[i].jStoreReady();
+		}
+	}
+};
+/**
+ * main loop responsible for ticking the ticker and starting ajax requesets.
+ */
 SONGTICKERLI.main = function() {
 	// initialize ticker
 	SONGTICKERLI.update(SONGTICKERLI.current_track());
+	window.setTimeout(function() {
+		$('#songtickerli .network-activity').show();
+		$.ajax({
+			url: '/data/rabe.ch/0.9.1/',
+			success: function(data) {
+				SONGTICKERLI.artist    = $(data).children('track').children('artist').text();
+				SONGTICKERLI.title     = $(data).children('track').children('title').text();
+				SONGTICKERLI.message   = $(data).children('track').children('message').text();
+				SONGTICKERLI.starttime = $(data).children('track').children('starttime').text();
+				SONGTICKERLI.delay = 6000;
+				SONGTICKERLI.main();
+			},
+			error: function() {
+				SONGTICKERLI.delay = 60000;
+				SONGTICKERLI.main();
+			}
+		});
+	}, SONGTICKERLI.delay);
+	if (SONGTICKERLI.delay == 0) {
+		SONGTICKERLI.delay = 6000;
+	}
 };
+/**
+ * update the ticker if changes are detected
+ */
 SONGTICKERLI.update = function(track) {
-	$('#songtickerli .artist').val(track.artist);
-	$('#songtickerli .title').val(track.title);
+	if (SONGTICKERLI.starttime == $('#songtickerli .starttime').html()) {
+		return;
+	}
+	// clone old data to history
+	SONGTICKERLI.update_history();
+
+	// load new data
+	if (track.artist) {
+		$('#songtickerli .overlay .artist').html(track.artist);
+		$('#songtickerli .overlay .title').html(track.title);
+	} else if (track.message) {
+		$('#songtickerli .overlay .artist').html(track.message);
+	} else {
+		$('#songtickerli .overlay .artist').html(track.title);
+	}
+
+	$('#songtickerli .overlay .starttime').html(SONGTICKERLI.starttime);
+};
+SONGTICKERLI.update_history = function() {
+	hist = $('#songtickerli .scroller-history');
+
+	overlay = $('#songtickerli .overlay').clone();
+	overlay.removeClass().addClass('history-data').css('background', 'white');
+	info    = $('#songtickerli .scroller-info').clone();
+	info.removeClass().addClass('history-info').appendTo($(overlay));
+	overlay.hide();
+
+	if ($(overlay).children('.artist').html() == '') {
+		return;
+	}
+
+	hist.prepend(overlay);
+	overlay.slideDown();
+	$('#songtickerli .scroller-history .button').remove();
 };
 SONGTICKERLI.love_track = function(current) {
 	if (!SONGTICKERLI.configured()) {
 		SONGTICKERLI.show($('#songtickerli .scroller-conf'));
 	}
-	if (SONGTICKERLI.showLoveDialog) {
+	if (typeof SONGTICKERLI.showLoveDialog == 'undefined' || SONGTICKERLI.showLoveDialog) {
 		SONGTICKERLI.show($('#songtickerli .scroller-love'));
+	} else {
+		SONGTICKERLI.observer_love(SONGTICKERLI.current_track());
 	}
+};
+SONGTICKERLI.love_track_okcallback = function() {
+	SONGTICKERLI.observer_love(SONGTICKERLI.current_track(), $('#songtickerli .love-message').val());
 };
 SONGTICKERLI.current_track = function() {
 	return {
-		artist: SONGTICKERLI.artist,
-		title:  SONGTICKERLI.title
+		artist:    SONGTICKERLI.artist,
+		title:     SONGTICKERLI.title,
+		message:   SONGTICKERLI.message,
+		starttime: SONGTICKERLI.starttime
 	}
 };
 SONGTICKERLI.show = function(choice) {
@@ -157,7 +274,11 @@ jQuery.jStore.ready(function(engine){
 
 	// radio button
 	$('#songtickerli input[value='+color+']:nth(0)').attr('checked','checked');
-	
+
+	// social networks
+	SONGTICKERLI.observer_jStoreReady();
+
+	// value_maps
 	for (var i = 0; i < SONGTICKERLI.configDataFields.length; i++) {
 		data = SONGTICKERLI.configDataFields[i];
 		$(data.field).val(jQuery.jStore.get(data.name));
@@ -175,8 +296,12 @@ jQuery(document).ready(function() {
 	$('#songtickerli .love').click(function() {
 		SONGTICKERLI.love_track(SONGTICKERLI.current_track());
 	});
+	$('#songtickerli button[name=post-love]').click(SONGTICKERLI.love_track_okcallback);
 	$('#songtickerli .info').click(function() {
 		SONGTICKERLI.show($('#songtickerli .scroller-info'));
+	});
+	$('#songtickerli .history').click(function() {
+		SONGTICKERLI.show($('#songtickerli .scroller-history'));
 	});
 	$('#songtickerli .conf').click(function() {
 		SONGTICKERLI.show($('#songtickerli .scroller-conf'));
