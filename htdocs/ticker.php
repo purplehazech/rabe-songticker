3a2902bad71fc9e9a5205d03850e2ecb8776e227
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-19 15:06:55 +0200

    fixed caching on this side

--- htdocs/ticker.php
+++ htdocs/ticker.php
@@ -10,14 +10,28 @@
  * since rabe.ch has an allow rule on its ip.
  */
 
-$res = file_get_contents("http://intranet.rabe.ch/songticker/0.9.3/test.php");
+if (array_key_exists('HTTP_IF_NONE_MATCH', $_SERVER)) {
+    $ifNoneMatch = $_SERVER['HTTP_IF_NONE_MATCH'];
+    $header = 'If-None-Match: '.$ifNoneMatch."\r\n";
+} else {
+    $header = '';
+}
+$ctxt = stream_context_create(
+    array(
+        'http' => array(
+            'method' => 'GET',
+            'header' => $header
+        )
+    )
+);
+
+$res = file_get_contents("http://intranet.rabe.ch/songticker/0.9.3/test.php", false, $ctxt);
 foreach ($http_response_header AS $header) {
     if (substr($header, 0, 5) == 'ETag:') {
         $etag = substr($header, 6);
     }
 }
 
-
 header('Content-Type: application/xml');
-header('Etag: '.$etag);
+header('ETag: '.$etag);
 echo $res;
