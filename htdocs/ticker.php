87c7bb4c783121ade241ace75a953a50928290fb
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-19 12:39:08 +0200

    switch to test url for now

--- htdocs/ticker.php
+++ htdocs/ticker.php
@@ -2,4 +2,4 @@
 // quick n dirty proxy
 
 header('Content-Type: application/xml');
-echo file_get_contents("http://intranet.rabe.ch/songticker/0.9.3/current.xml");
+echo file_get_contents("http://intranet.rabe.ch/songticker/0.9.3/test.php");
