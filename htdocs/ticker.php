52047e034b1012014c5392ee255037a57c95181a
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-19 12:45:27 +0200

    some more dox in pseuodoproxy

--- htdocs/ticker.php
+++ htdocs/ticker.php
@@ -1,5 +1,14 @@
 <?php
-// quick n dirty proxy
+/**
+ * a very dirty proxy
+ *
+ * i will start changing this so it passes on
+ * any proxy info coming from the browser or
+ * elsewhere, for now at least this works for 
+ * testing.
+ * For rabe.ch this will not need apikey supprt
+ * since rabe.ch has an allow rule on its ip.
+ */
 
 header('Content-Type: application/xml');
 echo file_get_contents("http://intranet.rabe.ch/songticker/0.9.3/test.php");
